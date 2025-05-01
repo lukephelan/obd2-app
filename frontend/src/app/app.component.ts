d2456ac456e4c75faa4b573ad6ca3fe1fcb420eb
Luke Phelan <luke.phelan@hotmail.com>
2025-05-02 07:55:03 +1000

    add a hello world api endpoint

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { HelloService } from './services/hello.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [AppLayoutComponent],
+  imports: [AppLayoutComponent, HttpClientModule],
+  providers: [HelloService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'frontend';
+  message: string = '';
+
+  constructor(private helloService: HelloService) {}
+
+  ngOnInit() {
+    this.helloService.getHello().subscribe(response => {
+      this.message = response.message;
+      console.log(response);
+    });
+  }
 }
